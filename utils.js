cdb476118da6787d7e616563ce550daa780d3a58
Samuel Atuma <atumasaake@gmail.com>
2022-07-17 06:55:42 +0100

    Removed redundant code lines

--- utils.js
+++ utils.js
@@ -100,45 +100,5 @@ function SplitEntity(entity) {
     return {ID: ID, Balance: balance, SplitBreakdown: splitResult};
 }
 
-const caseErr = {
-    "ID": 13092,
-    "Amount": 4500,
-    "Currency": "NGN",
-    "CustomerEmail": "anon8@customers.io",
-    "SplitInfo": [
-        {
-            "SplitType": "FLAT",
-            "SplitValue": 4,
-            "SplitEntityId": "LNPYACC0019"
-        },
-        {
-            "SplitType": "RATIO",
-            "SplitValue": 3,
-            "SplitEntityId": "LNPYACC0011"
-        },
-        {
-            "SplitType": "PERCENTAGE",
-            "SplitValue": 3,
-            "SplitEntityId": "LNPYACC0015"
-        },
-        {
-            "SplitType": "RATIO",
-            "SplitValue": 2,
-            "SplitEntityId": "LNPYACC0016"
-        },
-        {
-            "SplitType": "FLAT",
-            "SplitValue": 2450,
-            "SplitEntityId": "LNPYACC0029"
-        },
-        {
-            "SplitType": "PERCENTAGE",
-            "SplitValue": 10,
-            "SplitEntityId": "LNPYACC0215"
-        }
-    ]
-}
 
-// console.log()#
-// Export SplitEntity
 module.exports = SplitEntity
